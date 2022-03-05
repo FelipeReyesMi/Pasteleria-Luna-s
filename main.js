ee8646eae3e457943f8a899fab079c0f0ad53e3c
FelipeReyesMi <89532512+FelipeReyesMi@users.noreply.github.com>
2022-03-05 13:15:16 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector(".navbar").addEventListener("click", () => {
+    document.querySelector(".logo").classList.toggle("show");
+  });
+
+
+ScrollReveal().reveal('.apartado1', { delay: 550 });
+ScrollReveal().reveal('.logo', { delay: 550 });
+ScrollReveal().reveal('.mostrador', { delay: 550 });
+ScrollReveal().reveal('.container-footer-all', { delay: 550 });
+ScrollReveal().reveal('.box', { delay: 550 });
+ScrollReveal().reveal('.pasteles', { delay: 550 });
+ScrollReveal().reveal('.ingredientes', { delay: 550 });
